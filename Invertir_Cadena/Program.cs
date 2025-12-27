16f6567280ab9503dc72422dab787bf44460e4f7
giang <giang@GIAN>
2025-12-26 22:13:44 -0500

    Agregar archivos de proyecto.

---
+++ Invertir_Cadena/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Invertir una cadena
+Pide una palabra y muéstrala invertida.
+
+Ejemplo:
+hola → aloh
+
+No uses Reverse()
+*/
+namespace Invertir_Cadena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra");
+            string palabra = Console.ReadLine();
+
+            char[] letras = new char[palabra.Length];
+
+            for(int i = 0; i < palabra.Length; i++)
+            {
+                letras[i] = palabra[i];
+            }
+
+            for (int j = letras.Length - 1; j >= 0; j--)
+            {
+                Console.Write(letras[j]);
+            }
+        }
+    }
+}
